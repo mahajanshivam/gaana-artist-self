546e2b03861c15df0ccb10b19c0b7f11323c8145
mahajanshivam <mahajan_shivam@rocketmail.com>
2025-12-28 06:53:08 +0530

    Revert "- Creation of core modules"
    This reverts commit 51fb47ba9515247ec4b9b38b5cc0659e19462c0c.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,14 +29,4 @@ include(":build-logic")
 
 rootProject.name = "GaanaArtistSelf"
 include(":app")
-include(":core:common")
-include(":core:data")
-include(":core:database")
-include(":core:datastore")
-include(":core:datastore-proto")
-include(":core:model")
-include(":core:domain")
-include(":core:network")
-include(":core:navigation")
-include(":core:designsystem")
-include(":core:ui")
+ 
